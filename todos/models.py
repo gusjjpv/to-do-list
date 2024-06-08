a789e19aeaa4d31f5b90a8a14e3f2a17dde8b914
gusDev <joaogustavolima084@gmail.com>
2024-06-08 11:16:31 -0300

    feat:'add task function'

--- todos/models.py
+++ todos/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(verbose_name="Título",max_length=100, null=False, blank=False)
     # null=False vai obrigar o campo a ser preenchido, blank=False vai obrigar o campo a ser preenchido, n aceitando espaços em branco.
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     # auto_now_add=True vai adicionar a data e hora atual quando o objeto for criado.
-    deadline = models.DateTimeField(null=True, blank=True)
-    finishead = models.DateTimeField(null=True)
+    deadline = models.DateField(verbose_name="Data de Entrega",null=True, blank=True)
+    finishead = models.DateField(null=True)
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+
+from .models import Todo
 
 
-def home(request):
-    return render(request, "todos/home.html")
+class TodoListView(ListView):
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ["title", "deadline"]
+    success_url = reverse_lazy("todo_list")

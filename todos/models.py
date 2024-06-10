712c1ef9fe44700638f3d16f8047c69b4de36e5e
gusDev <joaogustavolima084@gmail.com>
2024-06-10 14:59:02 -0300

    feat: 'Complete and delete task function'

--- todos/models.py
+++ todos/models.py
@@ -1,10 +1,22 @@
+from datetime import date
+
 from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(verbose_name="Título",max_length=100, null=False, blank=False)
+    title = models.CharField(
+        verbose_name="Título", max_length=100, null=False, blank=False
+    )
     # null=False vai obrigar o campo a ser preenchido, blank=False vai obrigar o campo a ser preenchido, n aceitando espaços em branco.
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     # auto_now_add=True vai adicionar a data e hora atual quando o objeto for criado.
-    deadline = models.DateField(verbose_name="Data de Entrega",null=True, blank=True)
+    deadline = models.DateField(verbose_name="Data de Entrega", null=True, blank=True)
     finishead = models.DateField(null=True)
+    
+    class Meta:
+        ordering = ["deadline"]
+        
+    def mark_has_complete(self):
+        if not self.finishead:
+            self.finishead = date.today()
+            self.save()
